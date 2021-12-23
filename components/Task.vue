ce9921a894e7c0f090ae9ac4e6e40ab27f5fc5de
YAKorolev <korolev.ya.v@gmail.com>
2021-12-23 22:29:54 +0300

    add demo UI

---
+++ components/Task.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.Task 
+    .Task-name {{task.name}}
+    .Task-controls
+        button Изменить
+        button Удалить
+</template>
+
+<script>
+export default {
+    props: ["task"]
+}
+</script>
+
+<style lang="sass">
+.Task
+    display: flex
+    justify-content: space-between
+    padding: 5px 0
+    &-controls > button
+       margin-left: 10px 
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.Todo
+    .Todo-header
+        h1 Дела:
+        button Новое дело
+    hr
+    .Todo-list
+        Task.Todo-task(v-for="item in todo" :task="item")
+
+</template>
+
+<script>
+import Task from "../components/Task.vue"
+export default {
+    data() {
+        return {
+            todo: [ {name: "Принять ванну"}, {name: "Выпить чашечку кофе"} ]
+        }
+    },
+    components: {
+        Task
+    }
+}
+</script>
+
+<style lang="sass">
+.Todo
+    padding: 0 10px
+    &-header
+        display: flex
+        justify-content: space-between
+</style>
