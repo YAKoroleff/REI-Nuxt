caa0eb6a466d76ff14856358af47fb03c72466a4
YAKorolev <korolev.ya.v@gmail.com>
2022-01-08 09:48:44 +0300

    add tags ui

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,16 +1,12 @@
 <template lang="pug">
-.Task 
-    .Task-name(v-if="!isEdit") {{task.name}}
-    input.Task-name(v-if="isEdit" v-model="task.name")
-    .Task-controls
-        button(v-if="!isEdit" @click="isEdit = true") Редактировать
-        button(v-if="isEdit" @click="save") Сохранить
+.Tag 
+    .Tag-name {{tag.name}} 
         button(@click="remove") Удалить
 </template>
 
 <script>
 export default {
-    props: ["task"],
+    props: ["tag"],
     data ()
     {
         return {
@@ -19,12 +15,6 @@ export default {
     },
     methods:
     {
-        save ()
-        {
-            this.task.save()
-            this.isEdit = false
-        },
-
         remove ()
         {
             this.task.delete()
@@ -34,11 +24,10 @@ export default {
 </script>
 
 <style lang="sass">
-.Task
+.Tag
     display: flex
     justify-content: space-between
     padding: 5px 0
     &-controls > button
        margin-left: 10px 
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
-.Todo
-    .Todo-header
-        h1 Дела:
-        .NewTask
-            input.NewTask-name(v-model="newTaskName")
-            button.NewTask-save(@click="addTask") Новое дело
-    hr
-    .Todo-list
-        Task.Todo-task(v-for="item in todo" :task="item")
+.Desktop
+    .Todo
+        .Todo-header
+            h1 Дела:
+            .NewTask
+                input.NewTask-name(v-model="newTaskName")
+                button.NewTask-save(@click="addTask") Новое дело
+        hr
+        .Todo-list
+            Task.Todo-task(v-for="item in todo" :task="item")
+    .Tags
+        .Tags-header
+            h1 Теги:
+            .NewTag
+                input.NewTag-name(v-model="newTagName")
+                button.NewTag-save(@click="addTag") Новый тег
+        hr
+        .Tags-list
+            Tag.Todo-task(v-for="item in tags" :tag="item")
 
 </template>
 
 <script>
 import Task from "../components/Task.vue"
+import Tag from "../components/Tag.vue"
 import {tasks} from "../app/data"
 
 
@@ -20,7 +31,12 @@ export default {
     data() {
         return {
             newTaskName: '',
-            todo: tasks.items
+            newTagName: '',
+            todo: tasks.items,
+            tags: [
+                {name: 'Домашие дела'},
+                {name: 'Работа'},
+            ]
         }
     },
 
@@ -33,22 +49,41 @@ export default {
         addTask () {
             tasks.create({name: this.newTaskName})
             this.newTaskName = ''
+        },
+        addTag () {
+            this.newTagName = ''
         }
     },
 
     components: {
-        Task
+        Task, Tag
     }
 }
 </script>
 
 <style lang="sass">
+.Desktop
+    display: flex
+
 .Todo
+    flex-grow: 3
     padding: 0 10px
     &-header
         display: flex
         justify-content: space-between
 .NewTask 
+    & > *
+        height: 100%
+        box-sizing: border-box
+
+.Tags
+    background: #f3f3f3
+    flex-grow: 1
+    padding: 0 10px
+    &-header
+        display: flex
+        justify-content: space-between
+.NewTag
     & > *
         height: 100%
         box-sizing: border-box
